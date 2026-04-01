508b3e31273e0ed75bec56ce5ffd041ac6f3756f
Corean Namja Al Fathir <coreanfathir6@gmail.com>
2026-04-02 03:44:19 +0700

    Quiz 3 finished

--- exercises/018_functions.zig
+++ exercises/018_functions.zig
@@ -11,7 +11,7 @@
 //
 // Note the input parameter 'n' and return types are both u8.
 //
-const std = @import("std");
+const std: type = @import("std");
 
 pub fn main() void {
     // The new function deepThought() should return the number 42. See below.
@@ -25,6 +25,6 @@ pub fn main() void {
 // We're just missing a couple things. One thing we're NOT missing is the
 // keyword "pub", which is not needed here. Can you guess why?
 //
-??? deepThought() ??? {
+fn deepThought() u8 {
     return 42; // Number courtesy Douglas Adams
 }
